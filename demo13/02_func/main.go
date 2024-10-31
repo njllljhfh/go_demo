8b31945ffc421a6e9ef9d5badcf8b46e96b19980
njl <niujinlong@matrixtime.com>
2024-10-31 18:31:59 +0800

    map

--- demo13/02_func/main.go
+++ demo13/02_func/main.go
@@ -29,6 +29,25 @@ func sortIntDesc(slice []int) []int {
 	return slice
 }
 
+// map作为参数
+// 按照map中的key进行排序
+func mapSort(map1 map[string]string) string {
+	sliceKey := make([]string, 0)
+	// 把map的key放入切片中
+	for k, _ := range map1 {
+		sliceKey = append(sliceKey, k)
+	}
+	// 对key排序
+	sort.Strings(sliceKey)
+
+	str := ""
+	for _, v := range sliceKey {
+		str += fmt.Sprintf("%v=>%v ", v, map1[v])
+	}
+
+	return str
+}
+
 func main() {
 	slice := []int{12, 34, 37, 35, 556, 36, 2}
 	sortIntAsc(slice)
@@ -49,5 +68,13 @@ func main() {
 	sort.Ints(refSlice)
 	fmt.Println("Sorted numbers:", numbers)   // 输出: [5 1 3 4 2]
 	fmt.Println("Reference slice:", refSlice) // 输出: [1 3 4]
+	fmt.Println("------------------------------------------")
 
+	m1 := map[string]string{
+		"name": "张三",
+		"age":  "18",
+		"sex":  "男",
+	}
+	str1 := mapSort(m1)
+	fmt.Println(str1)
 }
