260031235e8c5eadb4cf510bb4eec8292b2a4794
njl <niujinlong@matrixtime.com>
2024-12-02 17:55:44 +0800

    文件读写

--- demo23/08_file_copy/main.go
+++ demo23/08_file_copy/main.go
@@ -10,63 +10,61 @@ import (
    复制文件操作
 */
 
-func CopyFile(srcFileName string, dstFileName string, fenMu int) (deferErr error) {
+func CopyFile(srcFileName string, dstFileName string, fenMu int) (err error) {
     // 处理未知异常
     defer func() {
-        if err := recover(); err != nil {
-            fmt.Printf("未知异常:%v\n", err)
+        if err1 := recover(); err1 != nil {
+            fmt.Printf("未知异常:%v\n", err1)
         }
     }()
 
-    sFile, deferErr := os.Open(srcFileName)
-    if deferErr != nil {
-        return
+    sFile, err2 := os.Open(srcFileName)
+    if err2 != nil {
+        return err2
     }
     // defer sFile.Close()
     defer func(f *os.File) {
-        err := f.Close()
-        if err != nil {
-            deferErr = fmt.Errorf("关闭src文件失败: %v\n", err)
+        err3 := f.Close()
+        if err3 != nil {
+            err = fmt.Errorf("关闭src文件失败: %v\n", err3)
         } else {
             fmt.Println("关闭src文件成功")
         }
     }(sFile)
 
-    dFile, deferErr := os.OpenFile(dstFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-    if deferErr != nil {
-        return
+    dFile, err4 := os.OpenFile(dstFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+    if err4 != nil {
+        return err4
     }
     // defer dFile.Close()
     defer func(f *os.File) {
-        err := f.Close()
-        // err = fmt.Errorf("模拟错误---关闭dst文件失败") // TODO: 模拟关闭报错
-        if err != nil {
-            deferErr = fmt.Errorf("关闭dst文件失败: %v\n", err)
+        err5 := f.Close()
+        // err5 = fmt.Errorf("模拟错误---关闭dst文件失败") // TODO: 模拟关闭报错
+        if err5 != nil {
+            err = fmt.Errorf("关闭dst文件失败: %v\n", err5)
         } else {
             fmt.Println("关闭dst文件成功")
         }
     }(dFile)
 
-    a := 1 / fenMu
+    a := 1 / fenMu // TODO: 模拟未知错误
     fmt.Printf("a = %v\n", a)
 
     tempSlice := make([]byte, 32)
     for {
         // 读取
-        n, err := sFile.Read(tempSlice)
-        if err == io.EOF {
+        n, err6 := sFile.Read(tempSlice)
+        if err6 == io.EOF {
             break
         }
-        if err != nil {
-            deferErr = fmt.Errorf("读取文件流失败: %v\n", err)
-            return
+        if err6 != nil {
+            return fmt.Errorf("读取文件流失败: %v\n", err6)
         }
 
         // 写入
-        _, err = dFile.Write(tempSlice[:n])
-        if err != nil {
-            deferErr = fmt.Errorf("写入文件流失败: %v\n", err)
-            return
+        _, err6 = dFile.Write(tempSlice[:n])
+        if err6 != nil {
+            return fmt.Errorf("写入文件流失败: %v\n", err6)
         }
     }
 
@@ -76,6 +74,7 @@ func CopyFile(srcFileName string, dstFileName string, fenMu int) (deferErr error
 func main() {
     src := "../test.txt"
     dst := "./copyFile.txt"
+    // fenMu := 0 // TODO: 模拟未知错误
     fenMu := 1
     err := CopyFile(src, dst, fenMu)
     if err != nil {
--- demo23/08_file_copy/main2.go
+++ demo23/08_file_copy/main2.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"os"
 )
 
 /*
@@ -11,63 +11,63 @@ import (
 */
 
 func mCopyFile(srcFileName string, dstFileName string) error {
-    // TODO: 这里在函数内定义 异常，是有问题的，go函数的return分两步！！！
-    // 	   第一步：将 finalErr 的值 赋值 给 函数底层维护的返回值
-    //     第二步：将 函数底层维护的返回值 返回给调用方
-    // 	   重点：defer函数执行的时机是在上述的第一步之后，在上述的第二步之前！！！
-    var finalErr error = nil
+	// TODO: 这里在函数内定义 异常，是有问题的，go函数的return分两步！！！
+	// 	   第一步：将 finalErr 的值 赋值 给 函数底层维护的返回值
+	//     第二步：将 函数底层维护的返回值 返回给调用方
+	// 	   重点：defer函数执行的时机是在上述的第一步之后，在上述的第二步之前！！！
+	var finalErr error = nil
 
-    defer func() {
-        if e := recover(); e != nil {
-            fmt.Printf("未知异常:%v\n", e)
-        }
-    }()
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("未知异常:%v\n", e)
+		}
+	}()
 
-    sFile, finalErr := os.Open(srcFileName)
-    defer func(f *os.File) {
-        err := f.Close()
-        if err != nil {
-            finalErr = fmt.Errorf("关闭src文件失败: %v\n", err)
-        } else {
-            fmt.Println("关闭src文件成功")
-        }
-    }(sFile)
+	sFile, finalErr := os.Open(srcFileName)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			finalErr = fmt.Errorf("关闭src文件失败: %v\n", err)
+		} else {
+			fmt.Println("关闭src文件成功")
+		}
+	}(sFile)
 
-    dFile, finalErr := os.OpenFile(dstFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-    defer func(f *os.File) {
-        err := f.Close()
-        if err != nil {
-            finalErr = fmt.Errorf("关闭dst文件失败: %v\n", err)
-        } else {
-            fmt.Println("关闭dst文件成功")
-        }
-    }(dFile)
+	dFile, finalErr := os.OpenFile(dstFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			finalErr = fmt.Errorf("关闭dst文件失败: %v\n", err)
+		} else {
+			fmt.Println("关闭dst文件成功")
+		}
+	}(dFile)
 
-    tempSlice := make([]byte, 32)
-    for {
-        // 读取
-        n, err1 := sFile.Read(tempSlice)
-        if err1 == io.EOF {
-            break
-        }
-        if err1 != nil {
-            return fmt.Errorf("读取文件流失败: %v\n", err1)
-        }
+	tempSlice := make([]byte, 32)
+	for {
+		// 读取
+		n, err1 := sFile.Read(tempSlice)
+		if err1 == io.EOF {
+			break
+		}
+		if err1 != nil {
+			return fmt.Errorf("读取文件流失败: %v\n", err1)
+		}
 
-        // 写入
-        _, finalErr = dFile.Write(tempSlice[:n])
-    }
+		// 写入
+		_, finalErr = dFile.Write(tempSlice[:n])
+	}
 
-    return finalErr
+	return finalErr
 }
 
 func main() {
-    src := "./test.txt"
-    dst := "./copyFile.txt"
-    err := mCopyFile(src, dst)
-    if err != nil {
-        fmt.Println("报错啦！！！ ", err)
-        return
-    }
-    fmt.Println("复制文件成功")
+	src := "./test.txt"
+	dst := "./copyFile.txt"
+	err := mCopyFile(src, dst)
+	if err != nil {
+		fmt.Println("报错啦！！！ ", err)
+		return
+	}
+	fmt.Println("复制文件成功")
 }
